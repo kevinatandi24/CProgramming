f4703112c78ff06e8ff0fa1a31050e5de9f86d34
KevinAtandi <kevinatandi24@gmail.com>
2025-05-05 00:58:08 +0300

    add both files

--- hello.c
+++ hello.c
@@ -1,5 +1,6 @@
 #include<stdio.h>
 int main() {
     printf("Hello, World!\n"); // Print "Hello, World!" to the console
+    printf("This is My First C Program.\n");
     return 0; // Return 0 to indicate successful execution
 }
