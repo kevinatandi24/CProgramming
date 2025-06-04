25cb6d1fe43c1447aa113c9fce11d11c624c69c3
KevinAtandi <kevinatandi24@gmail.com>
2025-06-04 03:06:43 +0300

    Functions

---
+++ Functions/Multiplication using functions.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void multi()
+{
+    int x,y=70,z=50;
+    x=y*z;
+    printf("Multi=%d",x);
+}
+int main()
+{
+    multi();
+}
---
+++ Functions/Subtraction using functions.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void sub()
+{
+    int x,y=70,z=50;
+    x=y-z;
+    printf("Sub=%d",x);
+}
+int main()
+{
+    sub();
+}
